72b7c8bbff51c1765e63299145aa10e768f57689
Guilherme Neves <guilhermevdneves@gmail.com>
2023-01-09 23:42:22 -0300

    Adds app routes

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from 'native-base';
+import React from 'react';
+
+function Profile() {
+  return (
+    <Center flex={1}>
+      <Text>Profile</Text>
+    </Center>
+  );
+}
+
+export default Profile
